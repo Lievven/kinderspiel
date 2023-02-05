a5b860857231cc705764d531a5dd084126c73b68
Lievven <opti8298@gmx.de>
2023-02-05 06:01:31 +0100

    beautified sprites implemented

--- src/kiddyboids/boids.rs
+++ src/kiddyboids/boids.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::*};
+use bevy::{prelude::*, sprite::{MaterialMesh2dBundle, self}, window::*};
 use std::{f32::consts::TAU};
 use rand::Rng;
 
@@ -15,8 +15,36 @@ const CENTERING_FACTOR: f32 = 0.0005;
 const MAX_SPEED: f32 = 400.;
 const MIN_SPEED: f32 = 150.;
 
-const BOID_COUNT: i32 = 500;
+const BOID_COUNT: i32 = 100;
+const RARE_CHANCE: f32 = 50.0;
 
+const ATLAS_GERMANY: &str = "boid_atlas_germany.png";
+const ATLAS_UKRAINE: &str = "boid_atlas_ukraine.png";
+const ATLAS_RAINBOW: &str = "boid_atlas_rainbow.png";
+const ATLAS_TRICOLORE: &str = "boid_atlas_tricolore.png";
+const ATLAS_STAR: &str = "boid_atlas_star.png";
+const ATLAS_FLOWER: &str = "boid_atlas_flower.png";
+const ATLAS_PLAIN: &str = "boid_atlas_plain.png";
+const ATLAS_KIEL: &str = "boid_atlas_kiel.png";
+
+const ATLAS_RARE: &'static [&'static str] = &[
+    "boid_atlas_germany.png",
+    "boid_atlas_ukraine.png",
+    "boid_atlas_rainbow.png",
+    "boid_atlas_kiel.png"
+];
+
+const ATLAS_COMMON: &'static [&'static str] = &[
+    "boid_atlas_tricolore.png",
+    "boid_atlas_flower.png",
+    "boid_atlas_plain.png",
+    "boid_atlas_star.png"
+];
+
+const SPRITE_SIZE: Vec2 = Vec2::new(256.0, 256.0);
+
+#[derive(Component, Deref, DerefMut)]
+pub struct AnimationTimer(Timer);
 
 // TODO: implement array of boids???
 #[derive(Resource, Deref, DerefMut)]
@@ -36,6 +64,80 @@ pub struct BoidId (usize);
 #[derive(Component)]
 enum Movement {
     Mouse,
+}
+
+pub fn boids_sprite_setup (
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut boids_list: ResMut<BoidsList>,
+) {
+    let mut rare_handles = Vec::with_capacity(ATLAS_RARE.len());
+    let mut common_handles = Vec::with_capacity(ATLAS_COMMON.len());
+
+    for i in 0..ATLAS_RARE.len() {
+        let texture_handler: Handle<Image> = asset_server.load(ATLAS_RARE[i]);
+        let texture_atlas = TextureAtlas::from_grid(
+            texture_handler, SPRITE_SIZE, 2, 2, None, None
+        );
+        rare_handles.push(texture_atlases.add(texture_atlas));
+    }
+
+    for i in 0..ATLAS_COMMON.len() {
+        let texture_handler: Handle<Image> = asset_server.load(ATLAS_COMMON[i]);
+        let texture_atlas = TextureAtlas::from_grid(
+            texture_handler, SPRITE_SIZE, 2, 2, None, None
+        );
+        common_handles.push(texture_atlases.add(texture_atlas));
+    }
+    
+    let mut rng = rand::thread_rng();
+    for i in 0..BOID_COUNT {
+        let radians = i as f32 * TAU / BOID_COUNT as f32;
+        let radius = 100.0 * rng.gen_range(0.1 .. 4.0);
+        let x = 400. + f32::sin(radians) * radius;
+        let y = 300. - f32::cos(radians) * radius;
+        boids_list.push(Boid{x, y, velocity_x: 0., velocity_y: 0.});
+    }
+    
+    for i in 0..boids_list.len() {
+        let rare_flip: f32 = rng.gen_range(0.0 .. 100.0);
+        let mut texture: Handle<TextureAtlas>;
+        if rare_flip < RARE_CHANCE {
+            let sprite_choice: usize = rng.gen_range(0..rare_handles.len());
+            texture = rare_handles[sprite_choice].clone();
+        } else {
+            let sprite_choice: usize = rng.gen_range(0..common_handles.len());
+            texture = common_handles[sprite_choice].clone();
+        }
+
+        commands.spawn((
+            SpriteSheetBundle {
+                texture_atlas:texture,
+                ..default()
+            },
+            BoidId(i),
+            AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating))
+        ));
+    }
+}
+
+pub fn animate_sprite (
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
 }
 
 pub fn boids_setup (
@@ -134,13 +236,14 @@ pub fn boid_movement (
 
     // apply boid positions to sprites
     for (boid_id, mut transform) in &mut boid_position {
+        info!("Transform: {:?}", transform);
         let mut boid = &mut boids_list[boid_id.0];
         boid.x += boid.velocity_x * time.delta_seconds();
         boid.y += boid.velocity_y * time.delta_seconds();
      
         let window = windows.get_primary().unwrap();
         transform.translation.x = boid.x - window.width() / 2.0;
-        transform.translation.y = boid.y - window.height() / 2.0;
+          transform.translation.y = boid.y - window.height() / 2.0;
         
     }
 }
